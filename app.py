4d3a93fe39d43b376b6fbf047104a4adeff89e90
Jerome <49881302+jswebpages@users.noreply.github.com>
2020-12-15 14:13:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, flash, session
 from random import randint, choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
-#help with mentor in Git
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "dancingmonkey2019"
